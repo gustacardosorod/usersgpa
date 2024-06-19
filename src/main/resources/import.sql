32d488d74b9198c7921477ada1bcc5e79d3feca1
gustavocardosorod <gustavocardoso1705@gmail.com>
2024-06-19 19:38:15 -0300

    Criando Repositorio do novo projeo

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_operator(name) VALUES ('Dr. Lucas');
+INSERT INTO tb_operator(name) VALUES ('Dra. Fernanda');
+
+INSERT INTO tb_user(operator_id, name, email) VALUES (1, 'Maria', 'maria@gmail.com');
+INSERT INTO tb_user(operator_id, name, email) VALUES (1, 'Bob', 'bob@gmail.com');
+INSERT INTO tb_user(operator_id, name, email) VALUES (2, 'Alex', 'alex@gmail.com');
+INSERT INTO tb_user(operator_id, name, email) VALUES (2, 'Ana', 'ana@gmail.com');
